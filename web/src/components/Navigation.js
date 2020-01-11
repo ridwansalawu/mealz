517ba41a9801646f32e0c40ca3f7a1d9e62eebec
ridwansalawu <reedysalawu@gmail.com>
2020-01-11 13:52:25 +1100

    svg again

--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -8,7 +8,7 @@ class Navigation extends React.Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link" to="/home"><img className="logo" alt="logo" src="./logo.svg"></img></Link>
+                <Link className="nav-link" to="/home"><img className="logo" alt="logo" src="../logo.svg"></img></Link>
               </li>
               <li className="nav-item">
               <Link className="nav-link menu-text" to="/search/">Search</Link>
